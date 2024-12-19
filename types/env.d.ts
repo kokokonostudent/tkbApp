af79c23138e379f568bf707412800ed2fcd7da41
yamauchi-ryuji <yamauchi.ryuji.20@shizuoka.ac.jp>
2024-12-19 23:16:59 +0900

    Declaration change

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,12 +1,8 @@
-declare module 'process' {
-    global {
-      namespace NodeJS {
-        interface ProcessEnv {
-          TEST_KEY: string;
-          API_KEY: string;
-          NODE_ENV?: string;
-        }
-      }
-    }
+declare namespace NodeJS {
+  interface ProcessEnv {
+    TEST_KEY: string;
+    API_KEY: string;
+    NODE_ENV?: string;
   }
+}
   
